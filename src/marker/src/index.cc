6c74fb2c02ed80345beea200593acaac868fedd0
aquapi <aquaplmc@gmail.com>
2023-06-12 17:13:49 +0700

    Update marker

--- src/marker/src/index.cc
+++ src/marker/src/index.cc
@@ -5,53 +5,58 @@
 #include <cstdlib>
 #include <filesystem>
 
+// Some constants
 const std::string binaryFile = "./index",
     outputFile = "./output.txt";
 
-bool compareFiles(const std::string& file1, const std::string& file2) {
+// Compare two output of files
+inline bool compareFiles(const std::string& file1, const std::string& file2) {
     std::ifstream f1(file1);
     std::ifstream f2(file2);
 
     std::string line1, line2;
-    while (std::getline(f1, line1) && std::getline(f2, line2)) {
-        if (line1 != line2) {
+    while (std::getline(f1, line1) && std::getline(f2, line2)) 
+        if (line1 != line2) 
             return false;
-        }
-    }
 
     return (f1.eof() && f2.eof());
 }
 
-bool compileCode(const std::string& codeFile) {
-    int compilationResult = std::system(("g++ " + codeFile + " -o index").c_str());
+// Compile the target file
+inline bool compileCode(const std::string& codeFile) {
+    int compilationResult = std::system(("g++ " + codeFile + " -o " + binaryFile).c_str());
     return (compilationResult == 0);
 }
 
-void deleteFile(const std::string& filePath) {
+// Remove a file from filesystem
+inline void deleteFile(const std::string& filePath) {
     std::filesystem::remove(filePath);
 }
 
-int extractTotalScore(const std::string& testPath) {
-    std::string propertiesPath = testPath + "/properties.txt";
-    std::ifstream propertiesFile(propertiesPath);
+// Get total score from a test
+inline int getTestScore(const std::string& testPath) {
+    std::ifstream propertiesFile(testPath + "/properties.txt");
     int totalScore = 0;
 
     if (propertiesFile.is_open()) {
         std::string line;
-        if (std::getline(propertiesFile, line)) {
+        if (std::getline(propertiesFile, line)) 
             totalScore = std::stoi(line);
-        }
     }
 
     return totalScore;
 }
 
+// Print usage for the CLI
+inline int printUsage(char* argv[]) {
+    std::cout << "Usage: " << argv[0] << " <code_file> <tests_directory>" << std::endl;
+    return 1;
+};
+
 int main(int argc, char* argv[]) {
     // Check the number of command line arguments
-    if (argc != 3) {
-        std::cout << "Usage: " << argv[0] << " <code_file> <tests_directory>" << std::endl;
-        return 1;
-    }
+    if (argc != 3) 
+        return printUsage(argv);
 
     std::string codeFile = argv[1];
     std::string testsDirectory = argv[2];
@@ -78,8 +83,8 @@ int main(int argc, char* argv[]) {
             std::string inputPath = testPath + "/input.txt";
             std::string expectedOutputPath = testPath + "/expected.txt";
 
-            // Extract the total score from the properties file
-            int testTotalScore = extractTotalScore(testPath);
+            // Extract the total score of the test from the properties file
+            int testTotalScore = getTestScore(testPath);
             totalPoints += testTotalScore;
 
             // Run the compiled code and redirect output to a file
